19d8ebc33a7a3d0f7615b57f599c2b3065b5a9a7
32426 <32426@DESKTOP-9C9FGJL>
2019-11-26 23:39:49 +0900

    오늘은 여기까지...

---
+++ movie2019/src/movie2019/admin/notice/NoticeDAO.java
@@ -0,0 +1,347 @@
+package movie2019.admin.notice;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class NoticeDAO {
+	DataSource ds;
+	Connection con;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	int result;
+	Boolean bool;
+
+	public NoticeDAO() {
+		try {
+			Context init = new InitialContext();
+			ds = (DataSource) init.lookup("java:comp/env/jdbc/OracleDB");
+
+		} catch (Exception ex) {
+			System.out.println("DB 연결 실패 : " + ex);
+		}
+	}
+	public boolean NoticeInsert(NoticeVO nv) {
+		try {
+			con = ds.getConnection();
+			String sql = "insert into NOTICE_TBL(NOTICE_NUM," 
+					+ "NOTICE_NAME, NOTICE_SUBJECT, "
+					+ "NOTICE_CONTENT, NOTICE_DATE) "
+					+ "values((select nvl(max(NOTICE_NUM),0)+1 from NOTICE_TBL)," 
+					+ "?,?,?,sysdate)";
+			pstmt = con.prepareStatement(sql);
+		//	pstmt.setInt(1, nv.getNOTICE_NUM());
+			pstmt.setString(1, nv.getNOTICE_NAME());
+			pstmt.setString(2, nv.getNOTICE_SUBJECT());
+			pstmt.setString(3, nv.getNOTICE_CONTENT());
+//			pstmt.setDate(4, nv.getNOTICE_DATE());
+	
+			result = pstmt.executeUpdate();
+			System.out.println("TESTdoa=>" + nv.getNOTICE_NAME());
+			if (result == 1) {
+				bool = true;
+			} else {
+				bool = false;
+			}
+		} catch (java.sql.SQLIntegrityConstraintViolationException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+		return bool;
+	}
+
+	public int getListCount() {
+		int x = 0;
+		try {
+			con = ds.getConnection();
+			pstmt = con.prepareStatement("select count(*) from NOTICE_TBL");
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				x = rs.getInt(1);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("getListCount() 에러: " + e);
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+		return x;
+	}
+
+	public List<NoticeVO> getNoticeList(int page, int limit) {
+		// page : 페이지
+		// limit : 페이지 당 목록의 수
+		// NOTICE_RE_REF desc, NOTICE_RE_SEQ asc에 의해 정렬한 것을
+		// 조건절에 맞는 rnum의 범위 만큼 가져오는 쿼리문입니다.
+
+		String sql = "select * from NOTICE_TBL"
+				+ "	where NOTICE_NUM>=? and NOTICE_NUM<=?";
+
+		List<NoticeVO> list = new ArrayList<NoticeVO>();
+		// 한 페이지당 10개씩 목록인 경우 1페이지, 2페이지, 3페이지, 4페이지...
+		int startrow = (page - 1) * limit + 1;
+		// 읽기 시작할 row 번호( 1 11 21 31 ...
+		int endrow = startrow + limit - 1;
+		// 읽을 마지막 row 번호(10 20 30 40 ...
+
+		try {
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, startrow);
+			pstmt.setInt(2, endrow);
+			rs = pstmt.executeQuery();
+
+			// DB에서 가져온 데이터를 VO객체에 담습니다.
+			while (rs.next()) {
+				NoticeVO notice = new NoticeVO();
+				notice.setNOTICE_NUM(rs.getInt("NOTICE_NUM"));
+				notice.setNOTICE_NAME(rs.getString("NOTICE_NAME"));
+				notice.setNOTICE_SUBJECT(rs.getString("NOTICE_SUBJECT"));
+				notice.setNOTICE_CONTENT(rs.getString("NOTICE_CONTENT"));
+				notice.setNOTICE_DATE(rs.getDate("NOTICE_DATE"));
+				list.add(notice);
+			}
+			return list; // 값을 담을 객체를 저장한 리스트를 호출한 곳으로 가져갑니다.
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("getListCount() 에러: " + e);
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
+
+	public void setReadCountUpdate(int num) {
+		String sql = "update NOTICE " + "set NOTICE_READCOUNT=NOTICE_READCOUNT+1 " + "where NOTICE_NUM=?";
+		try {
+
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, num);
+
+			pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("getReadCountUpdate() 에러: " + e);
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	public NoticeVO getDetail(int num) {
+		NoticeVO notice = null;
+		try {
+			con = ds.getConnection();
+			pstmt = con.prepareStatement("select * from NOTICE where NOTICE_NUM=?");
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				notice = new NoticeVO();
+				notice.setNOTICE_NUM(rs.getInt("NOTICE_NUM"));
+				notice.setNOTICE_NAME(rs.getString("NOTICE_NAME"));
+				notice.setNOTICE_SUBJECT(rs.getString("NOTICE_SUBJECT"));
+				notice.setNOTICE_CONTENT(rs.getString("NOTICE_CONTENT"));
+				// NOTICE.setNOTICE_DATE(rs.getDate("NOTICE_DATE"));
+			}
+			return notice;
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("getDetail() 에러: " + e);
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
+
+
+	public boolean isNoticeWriter(int num, String pass) {
+		String NOTICE_sql="select * from NOTICE where NOTICE_NUM=?";
+		
+		try {
+			con=ds.getConnection();
+			pstmt=con.prepareStatement(NOTICE_sql);
+			pstmt.setInt(1, num);
+			rs=pstmt.executeQuery();
+			if(rs.next())
+				if(pass.equals(rs.getString("NOTICE_PASS"))) {
+					return true;
+				}
+		}catch(SQLException ex) {
+			System.out.println("isNOTICEWriter() 에러"+ex);
+		}
+			finally {
+				if (rs != null) {
+					try {
+						rs.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if (pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if (con != null) {
+					try {
+						con.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		return false;
+	}
+
+	public Boolean NoticeModify(NoticeVO modifyNotice) {
+		String sql = "update NOTICE set NOTICE_SUBJECT=?, NOTICE_CONTENT=? where NOTICE_NUM=?";
+
+		try {
+			con=ds.getConnection();
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, modifyNotice.getNOTICE_SUBJECT());
+			pstmt.setString(2, modifyNotice.getNOTICE_CONTENT());
+			pstmt.setInt(3, modifyNotice.getNOTICE_NUM());
+			int result=pstmt.executeUpdate();
+			if(result==1)
+				return true;
+		}catch(SQLException ex) {
+			System.out.println("NOTICEModify() 에러"+ex);
+		}
+			finally {
+				if (rs != null) {
+					try {
+						rs.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if (pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if (con != null) {
+					try {
+						con.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		return false;
+	}
+
+}
---
+++ movie2019/src/movie2019/admin/notice/NoticeListAction.java
@@ -0,0 +1,97 @@
+package movie2019.admin.notice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class NoticeListAction implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		NoticeDAO boarddao=new NoticeDAO();
+		List<NoticeVO> noticelist=new ArrayList<NoticeVO>();
+		
+		int page=1;
+		int limit=10;
+		
+		if(request.getParameter("page")!=null) {
+			page=Integer.parseInt(request.getParameter("page"));
+		}
+		System.out.println("넘어온 페이지 = "+page);
+		
+		//총 리스트 수를 받아옵니다.
+		int listcount=boarddao.getListCount();
+		System.out.println("리스트 수"+listcount);
+		
+		//리스트를 받아옵니다.
+		noticelist = boarddao.getNoticeList(page,limit);
+		
+		/*
+		총 페이지 수 = 
+		(DB에 저장된 총 리스트의 수 + 한 페이지에서 보여주는 리스트의 수 -1)/한 페이지에서 보여주는 리스트의 수
+		
+		 * 예를 들어 한 페이지에서 보여주는 리스트의 수가 10개인 경우
+		예1) DB에 저장된 총 리스트의 수가 0이면 총 페이지수는 0 페이지
+		예2) DB에 저장된 총 리스트의 수가 (1~10)이면 총 페이지수는 1 페이지
+		예3) DB에 저장된 총 리스트의 수가 (11~20)이면 총 페이지수는 2 페이지
+		예4) DB에 저장된 총 리스트의 수가 (21~30)이면 총 페이지수는 4 페이지
+		 */
+		
+		int maxpage = (listcount+limit-1)/limit;
+		System.out.println("총 페이지수="+maxpage);
+		
+		/*
+		startpage:현재 페이지 그룹에서 맨 처음에 표시될 페이지 수를 의미합니다.
+				([1],[11],[21] 등...)
+		보여줄 페이지가 30개일 경우 [1][2][3]...[30]까지 다 표시하기에는 너무 많기 때문에 보통 한 페이지에는
+		10 페에지 정도까지 이동할 수 있게 표시합니다.
+		예) 페이지 그룹이 아래와 같은 경우
+			[1][2][3][4][5][6][7][8][9][10]
+			페이지그룹의 시작 페이지는 startpage에 마지막 페이지는 endpage에 구합니다.
+			
+			예로 1~10 페이지의 내용을 나타낼때는 페이지 그룹은
+			[1][2][3]..[10]로 표시되고
+			11~20페이지의 내용을 나타낼때는 페이지 그룹은
+			[11][12][13]..[20]까지 표시됩니다.
+		 */
+		int startpage=((page-1)/10)*10+1;
+		System.out.println("현재 페이지에 보여줄 시작 페이지 수 ="+startpage);
+		
+		//endpage:현재 페이지 그룹에서 보여줄 마지막 페이지 수([10],[20],[30] 등..)
+		int endpage=startpage+10-1;
+		System.out.println("현재 페이지에 보여줄 마지막 페이지 수"+endpage);
+		
+		/*
+		 * 마지막 그룹의 마지막 페이지 값은 최대 페이지 값입니다.
+		 * 예로 마지막 페이지 그룹이 [21]~[30]인 경우
+		 * 시작페이지(startpage=21)와 마지막페이지(endpage=30)
+		 * 이지만 최대 페이지(maxpage)가 25라면 [21]~[25]까지만 표시되도록 합니다.
+		 */
+		if(endpage>maxpage) endpage=maxpage;
+		request.setAttribute("page", page);//현재 페이지 수
+		request.setAttribute("maxpage", maxpage);//최대 페이지 수
+		
+		//현재 페이지에 표시할 첫 페이지 수
+		request.setAttribute("startpage", startpage);
+
+		//현재 페이지에 표시할 끝 페이지 수
+		request.setAttribute("endpage", endpage);
+		request.setAttribute("listcount", listcount);
+	
+		//해당 페이지의 글 목록을 갖고 있는 리스트
+		request.setAttribute("noticelist", noticelist);
+		
+		ActionForward forward=new ActionForward();
+		forward.setRedirect(false);
+		
+		//글 목록 페이지로 이동하기 위해 경로를 설정합니다.
+		forward.setPath("Page/AdminPage/Notice/NoticeList.jsp");
+		
+		return forward;	//BoardFrontController.java로 리턴됩니다.
+	}
+
+}
