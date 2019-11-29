c9bfaf75bde8062394565c09f5b79cb78de742eb
duswl13 <plain64@hanmail.net>
2019-11-29 12:21:47 +0900

    dzdz

--- movie2019/WebContent/sql/faq.sql
+++ movie2019/WebContent/sql/faq.sql
@@ -1,10 +1,25 @@
 DROP TABLE FAQ;
 
 CREATE TABLE FAQ(
-FAQ_ID NUMBER PRIMARY KEY,
+FAQ_NUMBER NUMBER PRIMARY KEY,
 FAQ_NAME VARCHAR2(40) REFERENCES  USERS (USER_ID),
 FAQ_SUBJECT VARCHAR2(100),
 FAQ_CONTENT VARCHAR2(300),
 FAQ_DATE DATE
 );	
 
+
+INSERT INTO FAQ VALUES((select nvl(max(FAQ_NUMBER),0) from FAQ)+1,
+'admin1','¿ÃΩŒ¿Ã∆Æ π∫∞°ø©?','∫∏Ω¥¿‘¥œ¥Ÿ.',sysdate
+);
+
+INSERT INTO FAQ VALUES((select nvl(max(FAQ_NUMBER),0) from FAQ)+1,
+'admin1','¿ÃΩŒ¿Ã∆Æ π∫∞°ø©?1','∫∏Ω¥¿‘¥œ¥Ÿ.1',sysdate
+);
+
+INSERT INTO FAQ VALUES((select nvl(max(FAQ_NUMBER),0) from FAQ)+1,
+'admin2','¿ÃΩŒ¿Ã∆Æ π∫∞°ø©?2','∫∏Ω¥¿‘¥œ¥Ÿ.2',sysdate
+);
+INSERT INTO FAQ VALUES((select nvl(max(FAQ_NUMBER),0) from FAQ)+1,
+'admin3','¿ÃΩŒ¿Ã∆Æ π∫∞°ø©?3','∫∏Ω¥¿‘¥œ¥Ÿ.3',sysdate
+);
