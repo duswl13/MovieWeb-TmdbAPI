68e96e79dac7eadde788640ba3692639a3bf0326
User <User@SONYS>
2019-12-03 17:43:02 +0900

    기도합니다.

--- movie2019/src/movie2019/login/db/Member.java
+++ movie2019/src/movie2019/login/db/Member.java
@@ -14,7 +14,6 @@ public class Member {
 
 	private String id;
 	private String password;
-	private String nickname;
 	private String email;
 	public String getId() {
 		return id;
@@ -28,12 +27,7 @@ public class Member {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getNickname() {
-		return nickname;
-	}
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
+
 	public String getEmail() {
 		return email;
 	}
