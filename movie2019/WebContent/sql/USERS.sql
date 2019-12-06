7dbccb4ba4b305055202e5b1148c29ebcbb7e313
duswl13 <plain64@hanmail.net>
2019-12-06 17:52:39 +0900

    dd

--- movie2019/WebContent/sql/USERS.sql
+++ movie2019/WebContent/sql/USERS.sql
@@ -1,16 +1,16 @@
 DROP TABLE USERS;
 
 CREATE TABLE USERS(
-USER_ID VARCHAR2(40) PRIMARY KEY,/*회원 아이디*/
-USER_PASS VARCHAR2(40),/*회원 비밀번호*/
-USER_EMAIL VARCHAR2(30),/*회원 이메일*/
-USER_JOIN_DATE DATE,/*가입날짜*/
-USER_PHONE VARCHAR2(30),/*회원 핸드폰번호*/
-USER_TYPE CHAR(1) CHECK (USER_TYPE IN('y', 'n')) /*관리자 여부 */
+USER_ID VARCHAR2(40) PRIMARY KEY,
+USER_PASS VARCHAR2(40),
+USER_EMAIL VARCHAR2(30),
+USER_JOIN_DATE DATE,
+USER_PHONE VARCHAR2(30),
+USER_TYPE CHAR(1) CHECK (USER_TYPE IN('y', 'n')) 
 );
 
 
-/* 컬럼명 바꾸는거
+/* 
  ALTER TABLE USERS MODIFY(USER_TYPE CHAR(1) CHECK (USER_TYPE IN('y', 'n')));
 */
 
@@ -19,6 +19,11 @@ INSERT INTO USERS VALUES('admin2','1234','admin2@voshu.com',sysdate,null,'y');
 INSERT INTO USERS VALUES('admin3','1234','admin3@voshu.com',sysdate,null,'y');
 INSERT INTO USERS VALUES('admin4','1234','admin4@voshu.com',sysdate,null,'y');
 
+
+
+INSERT INTO USERS VALUES('kim1234','1234','asdjkl13@naver.com',sysdate,null,'n');
+INSERT INTO USERS VALUES('plain64','1234','asdjkl13@naver.com',sysdate,null,'n');
+INSERT INTO USERS VALUES('asdjkl13','1234','asdjkl13@naver.com',sysdate,null,'n');
 INSERT INTO USERS VALUES('duswl13','1234','plain64@naver.com',sysdate,null,'n');
 INSERT INTO USERS VALUES('sonyeonsoo','1234','sonyeonsoo@naver.com',sysdate,null,'n');
 INSERT INTO USERS VALUES('jiyeon','1234','jiyeon@naver.com',sysdate,null,'n');
