e32b13fd369aa5dc00bb94621b404052751dd388
김건수 <@DESKTOP-OPVH3B7>
2019-12-08 19:29:16 +0900

    sadasdasdasd

---
+++ movie2019/src/movie2019/chu/parse/MovieInfoVO.java
@@ -0,0 +1,47 @@
+package movie2019.chu.parse;
+
+public class MovieInfoVO {
+	private String id;
+	private String title;
+	private String overview;
+	private String poster_path;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getOverview() {
+		return overview;
+	}
+
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
+
+	public String getPoster_path() {
+		return poster_path;
+	}
+
+	public void setPoster_path(String poster_path) {
+		this.poster_path = poster_path;
+	}
+
+	@Override
+	public String toString() {
+		return "MovieInfoVO [id=" + id + ", title=" + title + ", overview=" + overview + ", poster_path=" + poster_path
+				+ "]";
+	}
+
+}
